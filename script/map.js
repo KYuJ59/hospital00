892fb11fa0f2a6b05ea9d158d940143c0804c13d
kyuj <kyuj55959@gmail.com>
2025-04-25 12:00:57 +0900

    메인페이지1차완료

--- script/map.js
+++ script/map.js
@@ -1,22 +1,54 @@
 
 document.addEventListener('DOMContentLoaded',()=>{
+	const wrap=document.querySelector('#map_mapBox')
 	const panel=document.querySelector('.mapPanel')
 
-	let mWidth=panel.offsetWidth
-	let mHeight=panel.offsetHeight
-	map()
-	function map() {
-		mWidth=panel.offsetWidth
-		mHeight=panel.offsetHeight
+	// let mWidth=wrap.offsetWidth
+	// let mHeight=wrap.offsetHeight
+	// map()
+	// function map() {
+	// 	mWidth=panel.offsetWidth
+	// 	mHeight=panel.offsetHeight
+	// 	panel.innerHTML="";
+	// 	new daum.roughmap.Lander({
+	// 		"timestamp" : "1745288346030",
+	// 		"key" : "2nsax",
+	// 		"mapWidth" : mWidth,
+	// 		"mapHeight" : mHeight
+	// 	}).render();
+	// }
+
+	// window.addEventListener('resize',map)
+
+
+	const map=(w,h)=>{
 		panel.innerHTML="";
 		new daum.roughmap.Lander({
 			"timestamp" : "1745288346030",
 			"key" : "2nsax",
-			"mapWidth" : mWidth,
-			"mapHeight" : mHeight
+			"mapWidth" : w,
+			"mapHeight" : h
 		}).render();
 	}
 
-	window.addEventListener('resize',map)
+	let wW=null;
+
+	function mapSizing(){
+		if(window.innerWidth<=768){
+			if(wW!=768){
+				wW=768
+				map(715,787)
+			}
+		}else{
+			if(wW!="full"){
+				wW="full"
+				map(1200,574)
+			}
+		}
+	}
+
+	window.addEventListener('load',mapSizing)
+	window.addEventListener('resize',mapSizing)
+
 
 })
